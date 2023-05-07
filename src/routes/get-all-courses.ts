ec859ffb6f91589e0dc22ba5f33226b714260197
Manakal <manakal@randomsoftware.net>
2023-05-07 11:09:03 +0530

    feat: :sparkles: Added course get endpoint and default error handling mechanism

---
+++ src/routes/get-all-courses.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+import { logger } from "../uitls/logger";
+import { AppDataSource } from "../data-source";
+import { Course } from "../models/course.model";
+
+export const getAllCourses = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    logger.debug(`Called get all Courses endpoint`);
+    const courseRepository = AppDataSource.getRepository(Course);
+
+    const courses = await courseRepository
+      .createQueryBuilder("courses")
+      .leftJoinAndSelect("courses.lessons", "LESSONS")
+      .orderBy("courses.seqNo")
+      .getMany();
+
+    res.status(200).json({ courses });
+  } catch (err) {
+    logger.error(`Error Calling getAllCourses()`);
+    next(err);
+  }
+};
