01ba372d30eb44ee5d2ea286ecc3e49e3661e841
Manakal <manakal@randomsoftware.net>
2023-07-23 23:47:30 +0530

    feat: :sparkles: Added update course endpoint

--- src/routes/update-course.ts
+++ src/routes/update-course.ts
@@ -1,17 +1,33 @@
-// import { NextFunction, Request, Response } from "express";
-// import { logger } from "../uitls/logger";
+import { NextFunction, Request, Response } from "express";
+import { logger } from "../uitls/logger";
+import { isInteger } from "../uitls/utils";
+import { AppDataSource } from "../data/data-source";
+import { Course } from "../models/course.model";
 
-// export const UpdateCourse = async (
-//   req: Request,
-//   res: Response,
-//   next: NextFunction
-// ) => {
-//   try {
-//     logger.debug(`Called Update Course endpoint`);
+export const UpdateCourse = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    logger.debug(`Called Update Course endpoint`);
+    const courseId = req.params.courseId;
+    const changes = req.body;
 
-//     res.status(200).json({ courses });
-//   } catch (err) {
-//     logger.error(`Error Calling getAllCourses()`);
-//     next(err);
-//   }
-// };
+    if (!isInteger(courseId)) {
+      throw `Invalid course id ${courseId}`;
+    }
+
+    AppDataSource.createQueryBuilder()
+      .update(Course)
+      .set(changes)
+      .where("id=:courseId", { courseId })
+      .execute();
+    res
+      .status(201)
+      .json({ message: `Course ${courseId} was updated successfully` });
+  } catch (err) {
+    logger.error(`Error Calling getAllCourses()`);
+    next(err);
+  }
+};
